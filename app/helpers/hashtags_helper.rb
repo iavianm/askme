535c19d82fcde434b4f508b96a1902dcf5d1783a
iavianm <iavianm@mail.ru>
2021-12-20 11:12:42 +0300

    fix hashtag helper

--- app/helpers/hashtags_helper.rb
+++ app/helpers/hashtags_helper.rb
@@ -3,7 +3,7 @@ module HashtagsHelper
     content_words = content.split(" ")
     content_with_links = content_words.map do |word|
       if word.include?("#")
-        link_to word, hashtag_path(word.delete('#'))
+        link_to word, hashtag_path(word.downcase.delete('#'))
       else
         word
       end
