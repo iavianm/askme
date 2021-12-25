64493ab58b2a1734bc3b4870f66d7e3bc0eeb365
iavianm <iavianm@mail.ru>
2021-12-25 20:50:05 +0300

    refactor hashtag_helper

--- app/helpers/hashtags_helper.rb
+++ app/helpers/hashtags_helper.rb
@@ -1,17 +1,3 @@
-# module HashtagsHelper
-#   def render_with_hashtags(content)
-#     content_words = content.split(" ")
-#     content_with_links = content_words.map do |word|
-#       if word.include?("#")
-#         link_to word, hashtag_path(word.downcase.delete('#'))
-#       else
-#         word
-#       end
-#     end
-#     content_with_links.join(" ").html_safe
-#   end
-# end
-
 module HashtagsHelper
   def render_with_hashtags(content)
     content.gsub(/#[[:word:]-]+/) { |hashtag| link_to hashtag, hashtag_path(hashtag.downcase.delete('#')) }.html_safe
